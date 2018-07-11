acd27590927733e3f3cdad1f98050e9b624ea13b
DESKTOP-8LEUT5Q\datnh <datnh1612@gmail.com>
2018-07-12 06:15:23 +0700

    <12/7/2018> Create model

---
+++ BookStoreModel/Models/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStoreModel.Models
+{
+    [Table("Menu")]
+    public class Menu
+    {
+        [Key]
+        public string ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
