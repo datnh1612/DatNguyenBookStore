1cba10106a3e55f9b4170624b6131afa013df1f7
DESKTOP-8LEUT5Q\datnh <datnh1612@gmail.com>
2018-07-18 09:58:29 +0700

    abc

--- BookStoreModel/Models/ApplicationUser.cs
+++ BookStoreModel/Models/ApplicationUser.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +11,14 @@ namespace BookStoreModel.Models
 {
     public class ApplicationUser : IdentityUser
     {
+
+        // generate user identity async by input is a application user get by cookie
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
     }
 }
