bccf05f320d6e908c95eecb9f5d7a61af99e6dc5
DESKTOP-8LEUT5Q\datnh <datnh1612@gmail.com>
2018-07-23 10:06:19 +0700

    create modules angularJS for front-end

---
+++ BookStoreWeb/SPA/app.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../assets/admin/libs/angular/angular.js" />
+
+(function () {
+    //define module for routing, this is root module
+    angular.module('datnguyenbookstore', ['datnguyenbookstore.common']).config(config);
+
+    //inject services of angular-ui-router, this service use for config routing for this function below
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    //this is nearly same with angular 2 about config a controller,but change directive = url
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/SPA/component/home/homeView.html",
+            controller: "homeController"
+        });
+    }
+})
---
+++ BookStoreWeb/SPA/component/products/product.module.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    angular.module('datnguyenbookstore.products', ['datnguyenbookstore.common']).config(config);
+
+    config.$inject('$stateProvider', '$urlRouterProvider');
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: '/products',
+            templateUrl: '/SPA/component/products/productListView.html',
+            controller: 'productListViewController'
+        });
+    }
+});
